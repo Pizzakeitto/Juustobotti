5b04071b969c5b242d87527d7a3fb72eb4e10ac7
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-01-02 15:23:17 +0200

    find command, used for finding discord users by id

---
+++ commands/find.js
@@ -0,0 +1,23 @@
+const { description, execute } = require("./osu");
+
+module.exports = {
+    name: "find",
+    description: "Finds discord user by id",
+    execute(message, args) {
+        var bruh = async function () {
+            let name;
+            try{
+                var bruh2 = await message.client.users.fetch(args[0]);
+                if(bruh2){
+                    name = bruh2.tag;
+                }
+            } finally {
+                console.log(name + " is the bruh");
+                return name;
+            }
+        }
+        bruh().then(name => {
+            message.channel.send(name + " is the maf");
+        })
+    }
+}
