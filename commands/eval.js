bcd8d535c0b6e7c6f065992e747078f9d305232b
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-01-11 02:11:02 +0200

    Changed the message

--- commands/eval.js
+++ commands/eval.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Evaluate code from mesag',
     execute(message, args){
         if(!args){
-            return message.channel.send("No arguments given!")
+            return message.channel.send("Nothing to eval!")
         }
         const prefix = process.env.PREFIX;
         var newargs = message.content.slice(prefix.length + 4).trim();
