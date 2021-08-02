b538f87c6969d2ad823afc2faa7327608e35047b
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-08-02 22:28:02 +0000

    Initial version of Help command

---
+++ commands/utility/help.js
@@ -0,0 +1,18 @@
+const { prefix } = require('../../config.json')
+const Discord = require('discord.js')
+
+module.exports = {
+	name: 'help',
+	description: 'List all of my commands or info about a specific command.',
+	usage: '[command name]',
+	execute(message = new Discord.Message, args = []) {
+		const data = [];
+        const {commands} = message.client;
+        if (!args.length) {
+            data.push('Here\'s a list of me commands:');
+            data.push(commands.map(command => command.name).join(', '))
+            data.push(`\n(You can send '${prefix}help [command] to get more info about a specific command!)`);
+            message.channel.send(data)
+        }
+	}
+}
