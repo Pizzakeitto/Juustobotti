42213b2dd8db329a83d81a7ed477c82b7ba1b70f
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-08-02 22:27:14 +0000

    Made a bit more safe

--- commands/utility/eval.js
+++ commands/utility/eval.js
@@ -5,14 +5,18 @@ module.exports = {
         if(!args){
             return message.channel.send("Nothing to eval!")
         }
+
+        if (message.author.id != "246721024102498304") {
+            return message.channel.send("Youre not Pizzakeitto (Sorry i prefer leaving this command for me only) ?!?!")
+        }
         const {prefix} = require('../../config.json');
         var newargs = message.content.slice(prefix.length + 4).trim();
 
-        console.log(newargs);
         try {
             message.channel.send(eval(`${newargs}`));
         } catch (error){
             message.channel.send(`Didnt work: ${error}`);
         }
+
     }
 }
