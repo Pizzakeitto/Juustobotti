50011ba2c4d1af44f1c0728d409610981aef5ee2
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-08-05 21:21:21 +0000

    Got mostly rid of "cannot send empty message"

--- commands/utility/eval.js
+++ commands/utility/eval.js
@@ -1,7 +1,9 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'eval',
     description: 'Evaluate code from mesag',
-    execute(message, args){
+    execute(message = new Discord.Message, args = []){
         if(!args){
             return message.channel.send("Nothing to eval!")
         }
@@ -13,7 +15,10 @@ module.exports = {
         var newargs = message.content.slice(prefix.length + 4).trim();
 
         try {
-            message.channel.send(eval(`${newargs}`));
+            let evaled = eval(`${newargs}`)
+            if(evaled) {
+                message.channel.send(evaled).catch(err => {})
+            }
         } catch (error){
             message.channel.send(`Didnt work: ${error}`);
         }
