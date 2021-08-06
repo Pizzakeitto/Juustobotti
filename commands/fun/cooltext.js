5ca901215f2606bfe78f0f92f96f15d6fafcf21b
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-08-06 23:25:13 +0000

    Made cool text generator even cooler

--- commands/fun/cooltext.js
+++ commands/fun/cooltext.js
@@ -7,12 +7,22 @@ module.exports = {
     execute(message = new Discord.Message, args = []){
         const axios = require('axios')
 
+        if(args.length == 0) {
+            return message.channel.send("What text do I generate? lol")
+        }
+
+        message.channel.startTyping()
+
         let text = args.join(' ').toString()
-        console.log(text)
         axios.post('https://cooltext.com/PostChange', `LogoID=4&Text=${text}&FontSize=70&Color1_color=%23FF0000&Integer1=15&Boolean1=on&Integer9=0&Integer13=on&Integer12=on&BackgroundColor_color=%23FFFFFF`).then(res => {
             // console.log(res.data)
-            message.channel.send(res.data.renderLocation)
-            
+            // message.channel.send(res.data.renderLocation)
+            let httplocation = res.data.renderLocation.replace('https', 'http')
+            message.channel.send({
+                files: [`${httplocation}`]
+            })
+            message.channel.stopTyping()
         })
+        message.channel.stopTyping()
     }
 }
