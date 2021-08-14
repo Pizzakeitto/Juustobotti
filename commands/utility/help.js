a68cf2b5d367e3fc4e931fe893141f62fd070a5c
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-08-14 14:05:03 +0000

    Made hidden commands a thing

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -10,7 +10,7 @@ module.exports = {
         const {commands} = message.client;
         if (!args.length) {
             data.push('Here\'s a list of me commands:');
-            data.push(commands.map(command => command.name).join(', '))
+            data.push(commands.map(command => command.hidden ? '' : command.name).filter(n => n).join(', '))
             data.push(`\n(You can send '${prefix}help [command] to get more info about a specific command!)`);
             message.channel.send(data)
         }
