18880cd9c66740545a29bef2fbb9f7c50756891b
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-08-31 18:43:12 +0000

    Initial functionality

--- commands/fun/roulette.js
+++ commands/fun/roulette.js
@@ -4,22 +4,36 @@ module.exports = {
     description: 'Play a game of roulette!',
     execute(message = new Discord.Message, args = [""]) {
         // if no args gtfo
-        // if(args[0] == '') return message.channel.send('What you rouletting on? (its either a number or a color)')
-        var redNumbers   = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
-        var blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+        if(args[0] == null) return message.channel.send('What you rouletting on? (its either a number or a color)')
 
-        // Check if all numbers exist
-        var numbers = ''
-        for (var i = 0; i < 18; i++) {
-            if(redNumbers[i] < blackNumbers[i]) {
-                numbers += `${redNumbers[i]} `
-                numbers += `${blackNumbers[i]} `
-            } else {
-                numbers += `${blackNumbers[i]} `
-                numbers += `${redNumbers[i]} `
-            }
+        function send(msg) {
+            message.channel.send(msg)
         }
 
-        console.log(numbers)
+        var redNumbers   = ['1', '3', '5', '7', '9', '12', '14', '16', '18', '19', '21', '23', '25', '27', '30', '32', '34', '36']
+        var blackNumbers = ['2', '4', '6', '8', '10', '11', '13', '15', '17', '20', '22', '24', '26', '28', '29', '31', '33', '35']
+
+        var validChoises = []
+        validChoises.push('0')
+        redNumbers.forEach(number => validChoises.push(number))
+        blackNumbers.forEach(number => validChoises.push(number))
+        validChoises.push('red')
+        validChoises.push('black')
+        console.log(validChoises)
+
+        var startTimeMS = 0
+
+        if(validChoises.includes(args[0].toLowerCase())) {
+            startTimer(timer)
+            send(`OK You put your life on ${args[0]}! Good luck! (Game starts in ${timer})`)
+        }
+
+        function randomNumber(){
+            return Math.floor(Math.random() * 36)
+        }
+
+        function startTimer(timer){
+            startTimeMS = new Date().getTime // https://stackoverflow.com/questions/3700200/how-to-find-the-remaining-time-of-a-settimeout
+        }
     }
 }
