0fa3bd15c2bd27527efd65e10303870a737b104e
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-10-02 12:14:40 +0000

    Works again

--- commands/fun/cooltext.js
+++ commands/fun/cooltext.js
@@ -5,7 +5,8 @@ module.exports = {
     description: 'Generates some cool text!',
     usage: '',
     execute(message = new Discord.Message, args = []){
-        const axios = require('axios')
+        const https = require('https')
+        const axios = require('axios').default // The "default" is for intellisense and stuff
 
         if(args.length == 0) {
             return message.channel.send("What text do I generate? lol")
@@ -14,9 +15,12 @@ module.exports = {
         message.channel.startTyping()
 
         let text = args.join(' ').toString()
-        axios.post('https://cooltext.com/PostChange', `LogoID=4&Text=${text}&FontSize=70&Color1_color=%23FF0000&Integer1=15&Boolean1=on&Integer9=0&Integer13=on&Integer12=on&BackgroundColor_color=%23FFFFFF`).then(res => {
+        // Temporarily ignore SSL stuff (not secure this way but idc its just text)
+        const agent = new https.Agent({  
+            rejectUnauthorized: false
+        });
+        axios.post('https://cooltext.com/PostChange', `LogoID=4&Text=${text}&FontSize=70&Color1_color=%23FF0000&Integer1=15&Boolean1=on&Integer9=0&Integer13=on&Integer12=on&BackgroundColor_color=%23FFFFFF`, {httpsAgent: agent}).then(res => {
             // console.log(res.data)
-            // message.channel.send(res.data.renderLocation)
             let httplocation = res.data.renderLocation.replace('https', 'http')
             message.channel.send({
                 files: [`${httplocation}`]
