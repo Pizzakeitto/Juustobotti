87f1eb2a72af5da06b9cc2b5cce8999fe214b9ea
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-10-02 11:21:03 +0000

    Changed API, now its very fancy

--- commands/fun/dog.js
+++ commands/fun/dog.js
@@ -1,9 +1,32 @@
 const Discord = require('discord.js')
-const fetch = require('node-fetch')
 module.exports = {
     name: 'dog',
     description: 'doggo',
     execute(message = new Discord.Message, args = []){
-        fetch(`https://api.udit.gq/api/dogimage`).then(res => res.json()).then(res => message.channel.send(res.dogimage))
+        const https = require('https')
+        const apiurl = "api.thedogapi.com"
+        const apipath = "/v1/images/search"
+        const apikey = process.env.DOGAPIKEY
+
+        message.channel.startTyping()
+
+        https.get({
+            headers: { 'X-API-KEY': apikey},
+            hostname: apiurl,
+            path: `${apipath}?has_breeds=true&mime_types=jpg,png&sub_id=${message.author.username}&limit=1`
+        }, (res) => {
+            if(res.statusCode != 200) return message.channel.send('Something went wrong!')
+            let data = ""
+            res.on('data', (d) => {
+                data += d
+            })
+
+            res.on('end', () => {
+                data = JSON.parse(data)
+                let dogurl = data[0].url
+                message.channel.send({files: [dogurl]})
+                message.channel.stopTyping()
+            })
+        })
     }
 }
