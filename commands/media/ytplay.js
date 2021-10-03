7ccc61a306f892606626137c48283f356eeab932
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-10-03 02:35:45 +0000

    Now uses async and actually works

--- commands/media/ytplay.js
+++ commands/media/ytplay.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core')
 const axios = require('axios').default
+const eventEmitter = require('events')
 
 module.exports = {
     name: 'play',
@@ -15,10 +16,64 @@ module.exports = {
         // Credits to https://stackoverflow.com/a/37704433 !!!
         const urlRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
 
-        let ytvid = ""
+        let ytvid
+        let isUrl
+        let vidUrl
+
+        main()
+        
+        async function main() {
+            // Put everything here
+            if (urlRegex.test(args[0])) {
+                ytvid = ytdl(args[0], {filter: "audioonly"})
+                isUrl = false
+            } else {
+                isUrl = true
+                // Time to play with the yt api
+                let res = await axios.get('https://www.googleapis.com/youtube/v3/search', { 
+                    params: { 
+                        key: process.env.YTAPIKEY,
+                        q: args.join(" "),
+                        type: "video",
+                        maxResults: 1,
+                    }
+                })
+                console.log(`Got a response from yt saying ${res.status}, ${res.statusText}`)
+                console.log(res.data)
+                if (res.data.items.length == 0) return message.channel.send('YT said not found?')
+                vidUrl = "https://www.youtube.com/watch?v=" + res.data.items[0].id.videoId
+                console.log(vidUrl)
+                ytvid = ytdl(vidUrl, {filter: "audioonly"})
+            }
+    
+            // Join the vc
+    
+            let connection = await voicechannel.join()
+            if (process.env.WaitInChannel != undefined) clearTimeout(process.env.WaitInChannel)
+            if (isUrl) {
+                message.channel.send(`Now playing: \n\n${vidUrl}`)
+            }
+            await message.react('✅')
+            // Start playing the stream
+            const dispatcher = connection.play(ytvid)
+            dispatcher.on("close", close => {
+                // voicechannel.leave()
+            })
+
+            dispatcher.on("finish", finish => {
+                process.env.WaitInChannel = setTimeout( function () {
+                    voicechannel.leave()
+                    message.channel.send('Left due to inactivity 💀')
+                }, 30 * 1000 * 60 /* 30 minutes */)
+            })
+        }
+        return
+        // Keeping this here just in case
         if (urlRegex.test(args[0])) {
             ytvid = ytdl(args[0], {filter: "audioonly"})
+            isUrl = false
         } else {
+            isUrl = true
             // Time to play with the yt api
             axios.get('https://www.googleapis.com/youtube/v3/search', { 
                 params: { 
@@ -31,16 +86,21 @@ module.exports = {
                 console.log(`Got a response from yt saying ${res.status}, ${res.statusText}`)
                 console.log(res.data)
                 if (res.data.items.length == 0) return message.channel.send('YT said not found?')
-                let vidurl = "https://youtu.be/" + res.data.items[0].id.videoId
-                console.log(vidurl)
-                ytvid = ytdl(vidurl, {filter: "audioonly"})
+                vidUrl = "https://www.youtube.com/watch?v=" + res.data.items[0].id.videoId
+                console.log(vidUrl)
+                ytvid = ytdl(vidUrl, {filter: "audioonly"})
             }).catch(err => {
                 console.error(err)
             })
         }
 
         // Join the vc
+
         voicechannel.join().then(connection => {
+            if (process.env.WaitInChannel != undefined) clearTimeout(process.env.WaitInChannel)
+            if (isUrl) {
+                message.channel.send(`Now playing: \n\n${vidUrl}`)
+            }
             message.react('✅')
             // Start playing the stream
             const dispatcher = connection.play(ytvid)
@@ -49,10 +109,14 @@ module.exports = {
             })
 
             dispatcher.on("finish", finish => {
-                voicechannel.leave() // When song done, leave
+                process.env.WaitInChannel = setTimeout( function () {
+                    voicechannel.leave()
+                    message.channel.send('Left due to inactivity 💀')
+                }, 30 * 1000 * 60 /* 30 minutes */)
             })
         }).catch(err => {
-            message.reply('Cant join vc?')
+            message.reply('Error something happened (possibly cant join vc?)')
+            console.error(err)
         })
     }
 }
