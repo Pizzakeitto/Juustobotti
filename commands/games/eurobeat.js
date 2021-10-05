8eb53633c0c39f286a628a9ab2b0f87b0d9c1de4
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-10-05 16:35:40 +0000

    Eurobeat radio

---
+++ commands/games/eurobeat.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'eurobeat',
+    description: 'eurobeat',
+    execute(message = new Discord.Message, args = [""]) {
+        const voicechannel = message.member.voice.channel
+        if (!voicechannel) return message.reply('You need to join a voice channel first!')
+
+        main()
+
+        async function main(){
+            let connection = await voicechannel.join()
+            message.react('✅')
+            message.channel.send('Pls enjoy eurobeat')
+            const dispatcher = connection.play("http://165.227.19.100:9001/listen.aac") // Eurobeat radio, https://idforums.net/
+        }
+    }
+}
