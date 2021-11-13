c290382e921bc5a9ff2d663f3e707afef194dccb
Henri Lindqvist <henri.lindqvist@hotmail.com>
2021-11-13 20:56:10 +0000

    rewrite

--- commands/utility/find.js
+++ commands/utility/find.js
@@ -1,21 +1,15 @@
+const Discord = require('discord.js')
 module.exports = {
     name: "find",
     description: "Finds discord user by id",
-    execute(message, args) {
-        var bruh = async function () {
-            let name;
-            try{
-                var bruh2 = await message.client.users.fetch(args[0]);
-                if(bruh2){
-                    name = bruh2.tag;
-                }
-            } finally {
-                console.log(name + " is the bruh");
-                return name;
-            }
-        }
-        bruh().then(name => {
-            message.channel.send(name + " is the maf");
-        })
+    hidden: true,
+    async execute(message = new Discord.Message, args = []) {
+        const user = await message.client.users.fetch(`${args[0]}`)
+            .catch(err => {
+                message.channel.send(`Error: ${err.message}`)
+                return false
+            })
+        
+        if (user) message.channel.send(JSON.stringify(user, null, 2), {code: 'json'})
     }
 }
