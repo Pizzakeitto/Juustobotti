11d6faabe657ef341f2b97a31dd357197a91e8a2
Henri Lindqvist <henri.lindqvist@hotmail.com>
2022-01-09 17:52:45 +0000

    now works

--- commands/media/leave.js
+++ commands/media/leave.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js')
+const { getVoiceConnection } = require('@discordjs/voice')
 
 module.exports = {
     name: 'leave',
     description: 'leave a voice',
     execute(message = new Discord.Message, args = [""]) {
-        message.guild.voice.channel.leave()
+        getVoiceConnection(message.channel.guild.id).destroy()
         message.react('✅')
     }
 }
