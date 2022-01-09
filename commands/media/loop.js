3ec300625a875ba61d828f1f5bf985cee4547956
Henri Lindqvist <henri.lindqvist@hotmail.com>
2022-01-09 17:52:51 +0000

    now works

--- commands/media/loop.js
+++ commands/media/loop.js
@@ -3,20 +3,20 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'loop',
     description: 'Toggle looping!!!',
-    execute(message = new Discord.Message, args = [""]) {
+    execute(message = Discord.Message.prototype, args = [""]) {
         const voicechannel = message.member.voice.channel
         if (!voicechannel) return message.reply('You need to join a voice channel first!')
-        if (message.guild.voice == undefined) return message.reply('What do I loop???')
-        if (message.member.voice.channelID != message.guild.voice.channelID) message.channel.send('You need to be in the same channel as i am!!!')
+        if (voicechannel.id != message.guild.me.voice.channelId) message.channel.send('You need to be in the same channel as i am!!!')
 
-        if (message.guild.voice.connection.loop == undefined) message.guild.voice.connection.loop = false
-        if (message.guild.voice.connection.loop == false) {
-            message.guild.voice.connection.loop = true
+        // IF ITS LOOPING message.guild.me.voice.channel.loop == TRUE !!!!!!!!!!!!!!
+        if (message.guild.me.voice.channel.loop == undefined) message.guild.me.voice.channel.loop = false
+        if (message.guild.me.voice.channel.loop == false) {
+            message.guild.me.voice.channel.loop = true
             message.reply('Ok im now looping !!')
             return
         }
-        if (message.guild.voice.connection.loop == true) {
-            message.guild.voice.connection.loop = false
+        if (message.guild.me.voice.channel.loop == true) {
+            message.guild.me.voice.channel.loop = false
             message.reply('Ok no longer looping!!')
             return
         }
