9f1b6b23184a44a8893040e26d70b5f59d0fb1b5
Henri Lindqvist <henri.lindqvist@hotmail.com>
2022-02-27 03:37:31 +0000

    English translation by OpenAI

--- commands/fun/annos.js
+++ commands/fun/annos.js
@@ -7,6 +7,7 @@
 const Discord = require('discord.js')
 const parser = require('@fast-csv/parse')
 const fs = require('fs')
+const axios = require('axios').default
 
 module.exports = {
     name: 'annos',
@@ -103,7 +104,29 @@ module.exports = {
                 dose += "g"
             }
             
-            message.channel.send(`Saat päivän annoksen ${ravitsemusNimet[randomComponent]} syömällä ${dose} ${food.FOODNAME}.`)
+            if(args[0] == "en") {
+                message.channel.sendTyping()
+                // Translated by IBM Cloud https://cloud.ibm.com/catalog/services/language-translator AINAKI KUNHA NORDEA HERÄIS VITTU
+                // https://cloud.ibm.com/registration/
+                axios.post('https://api.openai.com/v1/engines/text-davinci-001/completions', {
+                    "prompt": `Translate this from Finnish to English\n\nSaat päivän annoksen ${ravitsemusNimet[randomComponent]} syömällä ${dose} ${food.FOODNAME}.\n\n`,
+                    "temperature": 0.3,
+                    "max_tokens": 100,
+                    "top_p": 1,
+                    "frequency_penalty": 0,
+                    "presence_penalty": 0
+                }, 
+                {
+                    headers: {
+                        "Content-Type": "application/json", 
+                        "Authorization": `Bearer ${process.env.OPENAIKEY}`
+                }
+                }).then((res) => {
+                    message.channel.send(res.data.choices[0].text.trim())
+                    console.log(`The thing got back with ${res.status} and data:`)
+                    console.log(res.data)
+                })
+            } else message.channel.send(`Saat päivän annoksen ${ravitsemusNimet[randomComponent]} syömällä ${dose} ${food.FOODNAME}.`)
         })
 
         /**
