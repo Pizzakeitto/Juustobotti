7eb9a9fc6a515c9975fa305334c6d63555c692db
Henri Lindqvist <henri.lindqvist@hotmail.com>
2022-05-13 08:28:38 +0000

    Fixed

--- commands/osu/link.js
+++ commands/osu/link.js
@@ -2,60 +2,32 @@ module.exports = {
     name: 'link',
     description: 'Links your discord name with your osu! name',
     detailedDescription: 'You can save your osu!username for me to rember so you dont have to type your username for every osu related command!',
-    usage: 'link [osu!username]',
-    execute(message, args){
-        if(args[0] == null || undefined){
-            return message.channel.send("No arguments given!")
+    usage: 'link [your osu username]',
+    execute(message = Discord.Message.prototype, args = [""]){
+        if(args[0] == undefined){
+            return message.channel.send("Pls specify your osu username!!")
         }
-
-        const mysql = require('mysql')
-        const {sqlconnection} = require('../../config.json')
-
-        let con = mysql.createConnection(sqlconnection)
-        con.connect((err) => {
-            if (err) return console.error(err)
-            // console.log('Connected to MySQL')
-
-            con.query(`INSERT INTO players (discordid, osuname) VALUES ('${message.author.id}', '${args[0]}') ON DUPLICATE KEY UPDATE osuname='${args[0]}'`, (err, res, fields) => {
-                if(err) return console.error(err)
-
-                if(res.affectedRows >= 1) {
-                    message.channel.send("Yup, you're linked aight. Now do `ju!osu`, I dare you")
-                    console.log(message.author.tag + " got saved to the database")
-                    con.end()
-                } else {
-                    message.channel.send("Something unexpected happened, I think... You can try doing `ju!osu` anyway")
-                    console.log(res)
-                    con.end()
-                }
+        const mariadb = require('mariadb')
+        const { sqlconfig } = require('../../config.json')
+        const { v2, auth } = require('osu-api-extended')
+        
+        mariadb.createConnection(sqlconfig).then(async connection => {
+            await auth.login(process.env.OSUCLIENTID, process.env.OSUCLIENTSECRET)
+            const osuUser = await v2.user.get(args[0], "osu")
+            
+            const response = await connection.query(`INSERT INTO players (discordid, osuname, osuid) VALUES ('${message.author.id}', '${args[0]}', '${osuUser.id}') ON DUPLICATE KEY UPDATE osuname='${args[0]}', osuid='${osuUser.id}'`)
+            .catch(err => {
+                console.log(err)
             })
+            if(response.affectedRows >= 1) {
+                message.channel.send("Yup, you're linked aight. Now you can do `ju!osu` and `ju!rs` without having to type your username!")
+                console.log(message.author.tag + " got saved to the database")
+                connection.end()
+            } else {
+                message.channel.send("Something unexpected happened, I think... You can try doing `ju!osu` anyway")
+                console.log(response)
+                connection.end()
+            }
         })
-
-        // rip
-        // MongoClient.connect(dburl, {useUnifiedTopology: true}, function(err, client) {
-        //     //assert.strictEqual(null, err)
-        //     if(err != null){
-        //         return console.error(err)
-        //     }
-        //     console.log("Connected to mongo server")
-        // 
-        //     let userData = new Map()
-        //     userData.set("_id", message.author.id) //Discord user's id is being used
-        //     userData.set("osuname", args[0])
-        // 
-        //     let alreadyLinked = false
-        //     const db = client.db(dbname)
-        //     
-        //     db.collection("userLinks").insertOne(userData, function(err, res){
-        //         if(err) {
-        //             console.error(err)
-        //             message.channel.send('Some error ocurred (Already linked?)')
-        //             client.close()
-        //             return
-        //         }
-        //         message.channel.send('You was linked aight')
-        //         client.close()
-        //     })
-        // })
     }
 }
