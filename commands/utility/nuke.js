dd0b948a7a68aeac8f7ad14d8fdc366bbb087f43
Henri Lindqvist <henri.lindqvist@hotmail.com>
2022-07-22 10:26:54 +0000

    disclaimer

--- commands/utility/nuke.js
+++ commands/utility/nuke.js
@@ -6,5 +6,6 @@ module.exports = {
     detailedDescription: 'Ever wanted to nuke Sweden? With this command you can!\n\n||(For legal reasons this is a joke)||',
     execute(message = new Discord.Message, args = [""]) {
         message.channel.send(":bomb: :flag_se: :arrow_right: :boom:")
+        message.channel.send("||note: for legal reasons this is a bad joke||")
     }
 }
