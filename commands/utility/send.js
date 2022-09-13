846d8aa4af228f405c6e90bec6b5888b05050d84
Henri Lindqvist <henri.lindqvist@hotmail.com>
2022-09-13 07:25:41 +0000

    fix

--- commands/utility/send.js
+++ commands/utility/send.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'send',
 	description: 'send mesasge to a chanel',
     hidden: true,
-	async execute(message = new Discord.Message, args = []) {
+	async execute(message = Discord.Message.prototype, args = []) {
         if (message.author.id != '246721024102498304') return message.reply('Sorry bro youre not pizzaman')
         if (!args[0]) return message.reply('Bruh where do i send stuff to?')
         if (!args[1]) return message.reply('Bruh what do i send?')
@@ -12,7 +12,7 @@ module.exports = {
         const channel = await message.client.channels.fetch(args[0])
 
         if(!channel) return message.reply('Chanel doesnt exist!!!')   
-        if(!channel.isText()) return message.reply('This isnt a text channel!')
+        if(!channel.type == Discord.ChannelType.GuildText) return message.reply('This isnt a text channel!')
 
         args.shift()
         channel.send(args.join(' '))
