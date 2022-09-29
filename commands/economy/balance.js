c9b0f5414fceb9e6a357c3cb1383d5f93ff61ef7
Henri Lindqvist <henri.lindqvist@hotmail.com>
2022-09-29 11:30:52 +0000

    Initial commit

---
+++ commands/economy/balance.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+const { getUser } = require('../../utils/mongoUtils')
+const { currency } = require('../../config.json')
+
+module.exports = {
+	name: 'balance',
+    aliases: ['bal'],
+	description: 'Check your balance NOW!',
+	detailedDescription: 'You can check your wallet and your bank balance with this command 😁😁😁😁😁😁😁',
+	async execute(message = Discord.Message.prototype, args = []) {
+		const user = await getUser(message.author.id)
+		message.channel.send(`You have ${user.wallet}${currency} in your wallet`)
+    }
+}
