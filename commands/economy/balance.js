d2af022d9f2028f0b79bb674297d2ae26482d041
Henri Lindqvist <henri.lindqvist@hotmail.com>
2023-06-26 15:49:58 +0000

    + bank

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -9,6 +9,6 @@ module.exports = {
 	detailedDescription: 'You can check your wallet and your bank balance with this command 😁😁😁😁😁😁😁',
 	async execute(message = Discord.Message.prototype, args = []) {
 		const user = await getUser(message.author.id)
-		message.channel.send(`You have ${user.wallet}${currency} in your wallet`)
+		message.channel.send(`You have ${user.wallet}${currency} in your wallet and ${user.bank}${currency} in your bank.`)
     }
 }
